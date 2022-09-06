aa9feb7a53631e0760bf07281e9fd6ad1b8e8473
Basantraj Shakti <80774098+Basantrajshakti@users.noreply.github.com>
2022-09-06 19:03:14 +0530

    Transition testing

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const bar90 = document.querySelectorAll('.bar90');
+const bar85 = document.querySelectorAll('.bar85');
+const bar80 = document.querySelectorAll('.bar80');
+
+window.onload = () => {
+  console.log(bar90);
+}
+  bar90[0].style.width = '90%';
